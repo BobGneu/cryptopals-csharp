9b30964a3e9828b5d749dd76338f2cb8327b9569
Bob Chatman <bob.chatman@gmail.com>
2016-09-11 16:08:59 -0700

    Solution to Set 1 - Challenge 1

---
+++ src/Crypto/Translation.cs
@@ -0,0 +1,129 @@
+﻿namespace Crypto
+{
+    using System;
+    using System.Text;
+
+    public class Translation
+    {
+        #region To Base64 Encoding
+
+        public static string BytesToBase64(byte[] input)
+        {
+            return Convert.ToBase64String(input);
+        }
+
+        // ReSharper disable once InconsistentNaming
+        public static string ASCIIToBase64(string input)
+        {
+            return BytesToBase64(ASCIIToBytes(input));
+        }
+
+        public static string HexToBase64(string input)
+        {
+            var bytes = HexToBytes(input);
+
+            Console.WriteLine(bytes.ToString());
+
+            return Convert.ToBase64String(bytes);
+        }
+
+        #endregion  
+
+        #region To Byte Conversion
+
+        // ReSharper disable once InconsistentNaming
+        public static byte[] ASCIIToBytes(string input)
+        {
+            return Encoding.ASCII.GetBytes(input);
+        }
+
+        public static byte[] Base64ToBytes(string input)
+        {
+            return Convert.FromBase64String(input);
+        }
+
+        public static byte[] HexToBytes(string input)
+        {
+            var arr = new string[input.Length/2];
+
+            for (var i = 0; i < input.Length; i += 2)
+            {
+                arr[i/2] = input.Substring(i, 2);
+            }
+
+            return Array.ConvertAll(arr, s => Convert.ToByte(s, 16));
+        }
+
+        #endregion
+
+        #region To ASCII Conversion
+
+        // ReSharper disable once InconsistentNaming
+        public static string BytesToASCII(byte[] input)
+        {
+            var tmpString = new StringBuilder();
+
+            foreach (var character in input)
+            {
+                tmpString.Append(Convert.ToChar(character));
+            }
+
+            return tmpString.ToString();
+        }
+
+        // ReSharper disable once InconsistentNaming
+        public static string Base64ToASCII(string input)
+        {
+            return BytesToASCII(Convert.FromBase64String(input));
+        }
+
+        // ReSharper disable once InconsistentNaming
+        public static string HexToASCII(string input)
+        {
+            return BytesToASCII(HexToBytes(input));
+        }
+
+        #endregion
+
+        #region To HEX Conversion
+
+        public static string BytesToHex(byte[] input)
+        {
+            var tmpString = new StringBuilder();
+
+            foreach (var character in input)
+            {
+                tmpString.Append($"{character:X}");
+            }
+
+            return tmpString.ToString();
+        }
+
+        // ReSharper disable once InconsistentNaming
+        public static string ASCIIToHex(string input)
+        {
+            var tmpString = new StringBuilder();
+
+            foreach (var character in ASCIIToBytes(input))
+            {
+                tmpString.Append($"{character:X}");
+            }
+
+            return tmpString.ToString();
+        }
+
+        public static string Base64ToHex(string input)
+        {
+            var tmpString = new StringBuilder();
+
+            foreach (var character in Base64ToBytes(input))
+            {
+                tmpString.Append($"{character:X}");
+            }
+
+            return tmpString.ToString();
+        }
+
+        #endregion
+    }
+}
---
+++ test/Crypto.Tests/Set1/Challenge1.cs
@@ -0,0 +1,149 @@
+﻿namespace Crypto.Tests.Set1
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Challenge1
+    {
+        [Category("To Bytes")]
+        [TestCase(" ", new byte[] { 32 })]
+        [TestCase("!", new byte[] { 33 })]
+        [TestCase("A", new byte[] { 65 })]
+        [TestCase("Man", new byte[] { 77, 97, 110 })]
+        // ReSharper disable once InconsistentNaming
+        public void ShouldBeAbleToConvertASCIItoByteArray(string input, byte[] expected)
+        {
+            Assert.AreEqual(expected, Translation.ASCIIToBytes(input));
+        }
+
+        [Category("To ASCII")]
+        [TestCase(" ", new byte[] { 32 })]
+        [TestCase("!", new byte[] { 33 })]
+        [TestCase("A", new byte[] { 65 })]
+        [TestCase("Man", new byte[] { 77, 97, 110 })]
+        // ReSharper disable once InconsistentNaming
+        public void ShouldBeAbleToConvertByteArrayToASCII(string expected, byte[] input)
+        {
+            Assert.AreEqual(expected, Translation.BytesToASCII(input));
+        }
+
+        [Category("To Hex")]
+        [TestCase(new byte[] { 16 }, "10")]
+        [TestCase(new byte[] { 32 }, "20")]
+        [TestCase(new byte[] { 33 }, "21")]
+        [TestCase(new byte[] { 65 }, "41")]
+        [TestCase(new byte[] { 128 }, "80")]
+        [TestCase(new byte[] { 255 }, "FF")]
+        [TestCase(new byte[] { 77, 97, 110 }, "4D616E")]
+        public void ShouldBeAbleToConvertByteArrayToHexString(byte[] input, string expected)
+        {
+            Assert.AreEqual(expected, Translation.BytesToHex(input));
+        }
+
+        [Category("To Bytes")]
+        [TestCase(new byte[] { 16 }, "10")]
+        [TestCase(new byte[] { 32 }, "20")]
+        [TestCase(new byte[] { 33 }, "21")]
+        [TestCase(new byte[] { 65 }, "41")]
+        [TestCase(new byte[] { 128 }, "80")]
+        [TestCase(new byte[] { 255 }, "FF")]
+        [TestCase(new byte[] { 77, 97, 110 }, "4D616E")]
+        public void ShouldBeAbleToConvertHexStringToByteArray(byte[] expected, string input)
+        {
+            Assert.AreEqual(expected, Translation.HexToBytes(input));
+        }
+
+        [Category("To Base64")]
+        [TestCase(new byte[] { 65 }, "QQ==")]
+        [TestCase(new byte[] { 77, 97, 110 }, "TWFu")]
+        public void ShouldBeAbleToConvertByteArrayToBase64String(byte[] input, string expected)
+        {
+            Assert.AreEqual(expected, Translation.BytesToBase64(input));
+        }
+
+        [Category("To Bytes")]
+        [TestCase(new byte[] { 65 }, "QQ==")]
+        [TestCase(new byte[] { 77, 97, 110 }, "TWFu")]
+        public void ShouldBeAbleToConvertBase64StringToByteArray(byte[] expected, string input)
+        {
+            Assert.AreEqual(expected, Translation.Base64ToBytes(input));
+        }
+
+        [Category("To Hex")]
+        [TestCase("A", "41")]
+        [TestCase("Man", "4D616E")]
+        // ReSharper disable once InconsistentNaming
+        public void ShouldBeAbleToConvertASCIIiToHex(string input, string expected)
+        {
+            Assert.AreEqual(expected, Translation.ASCIIToHex(input));
+        }
+
+        [Category("To ASCII")]
+        [TestCase("A", "41")]
+        [TestCase("Man", "4D616E")]
+        // ReSharper disable once InconsistentNaming
+        public void ShouldBeAbleToConvertHexToASCII(string expected, string input)
+        {
+            Assert.AreEqual(expected, Translation.HexToASCII(input));
+        }
+
+        [Category("To Base64")]
+        [TestCase("41", "QQ==")]
+        [TestCase("4D616E", "TWFu")]
+        public void ShouldBeAbleToConvertHexToBase64(string input, string expected)
+        {
+            Assert.AreEqual(expected, Translation.HexToBase64(input));
+        }
+        
+        [Category("To Hex")]
+        [TestCase("41", "QQ==")]
+        [TestCase("4D616E", "TWFu")]
+        public void ShouldBeAbleToConvertBase64ToHex(string expected, string input)
+        {
+            Assert.AreEqual(expected, Translation.Base64ToHex(input));
+        }
+
+        [Category("To Base64")]
+        [TestCase("A", "QQ==")]
+        [TestCase("Man", "TWFu")]
+        public void ShouldBeAbleToConvertAsciiToBase64(string input, string expected)
+        {
+            Assert.AreEqual(expected, Translation.ASCIIToBase64(input));
+        }
+
+        [Category("To ASCII")]
+        [TestCase("A", "QQ==")]
+        [TestCase("Man", "TWFu")]
+        // ReSharper disable once InconsistentNaming
+        public void ShouldBeAbleToConverBase64ToASCII(string expected, string input)
+        {
+            Assert.AreEqual(expected, Translation.Base64ToASCII(input));
+        }
+
+        [Category("To Hex")]
+        [TestCase("H", "48")]
+        [TestCase("e", "65")]
+        [TestCase("l", "6C")]
+        [TestCase("o", "6F")]
+        [TestCase(" ", "20")]
+        [TestCase("W", "57")]
+        [TestCase("r", "72")]
+        [TestCase("d", "64")]
+        [TestCase("!", "21")]
+        public void ShouldBeAbleToEncodeAsciiToHex(string input, string expected)
+        {
+            var result = Translation.ASCIIToHex(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Category("To Hex"), Category("Goal")]
+        [TestCase("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d", "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t")]
+        public void ShouldBeAbleToChangeEncodingOfSampleProblem(string input, string expected)
+        {
+            var result = Translation.HexToBase64(input);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
