1d724bc954668ece8db4db94ec789fcbc49f19a5
Bob Chatman <bob.chatman@gmail.com>
2016-09-11 16:42:29 -0700

    cleanup

--- src/Crypto/Utility.cs
+++ src/Crypto/Utility.cs
@@ -1,29 +1,22 @@
 ﻿namespace Crypto
 {
-    using System;
-    using System.Linq;
-
     public class Utility
     {
+        // ReSharper disable once InconsistentNaming
         public static string XORHexStrings(string message, string mask)
         {
             return Translation.BytesToHex(XORBytes(Translation.HexToBytes(message), Translation.HexToBytes(mask)));
         }
 
+        // ReSharper disable once InconsistentNaming
         private static byte[] XORBytes(byte[] message, byte[] mask)
         {
             var result = new byte[message.Length];
 
             for (var ndx = 0; ndx < message.Length; ndx++)
             {
-                Console.WriteLine(message[ndx]);
-                Console.WriteLine(mask[ndx % mask.Length]);
-                Console.WriteLine((byte)(message[ndx] ^ mask[ndx % mask.Length]));
-
                 result[ndx] = (byte)(message[ndx] ^ mask[ndx%mask.Length]);
             }
-
-            Console.Write(String.Join(", ", result));
 
             return result;
         }
